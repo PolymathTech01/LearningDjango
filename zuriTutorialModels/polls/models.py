f7b6e1d70d1ba987038e95a3c4839b700ea64d35
PolymathTech01 <polymathalli@gmail.com>
2022-06-26 05:00:29 +0100

    make the Choices object more readable in the admin site

--- zuriTutorialModels/polls/models.py
+++ zuriTutorialModels/polls/models.py
@@ -6,6 +6,8 @@ from django.db import models
 
 class Question(models.Model):
     question_text: Optional[str] = models.CharField(max_length=200)
+    question_description = models.CharField(
+        max_length=200, default="basic description")
     pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -18,4 +20,4 @@ class Choices(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_answer
+        return f"{self.question} ---> {self.choice_answer}"
